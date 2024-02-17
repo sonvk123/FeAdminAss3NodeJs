f81bf56d86d7cf8f8091d020976a3d1d08de1bae
sonvk123 <70042474+sonvk123@users.noreply.github.com>
2024-02-17 15:27:17 +0700

    update axios

--- src/API/axiosClient.jsx
+++ src/API/axiosClient.jsx
@@ -3,10 +3,9 @@ import queryString from "query-string";
 
 
 let url =
-  process.env.REACT_APP_NODE_ENV === "production"
-    ? "https://ass3-nodejs-q5t8.onrender.com/admin"
-    : "http://localhost:5com/admin";
-
+process.env.REACT_APP_NODE_ENV === "production"
+  ? "https://beass3nodejs.onrender.com/admin"
+  : "http://localhost:5000/admin";
 
 const axiosClient = axios.create({
   baseURL: url,
